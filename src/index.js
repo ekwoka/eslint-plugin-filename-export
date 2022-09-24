0d9ad1ff23ee74f786b01472145758d2f51206b6
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-09-24 12:55:45 +0400

    :bug: Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-const { basename, extname } = require("node:path");
+const { basename, extname } = require('node:path');
 
 module.exports = {
   rules: {
-    "match-named-export": {
+    'match-named-export': {
       create: function (context) {
         return {
           Program: function (node) {
             const filename = context.getFilename();
             const filenameSansExt = basename(filename, extname(filename));
-            if (filenameSansExt.toLowerCase() === "index") return;
+            if (filenameSansExt.toLowerCase() === 'index') return;
             const matchingExport = node.body.find((item) => {
-              if (item.type !== "ExportNamedDeclaration") return false;
-              const name = item.declaration.declarations[0].id.name;
-              if (name.toLowerCase() === filenameSansExt.toLowerCase())
-                return true;
+              if (item.type !== 'ExportNamedDeclaration') return false;
+              const name = item.declaration?.declarations?.[0]?.id?.name || '';
+              if (name.toLowerCase() === filenameSansExt.toLowerCase()) return true;
               return false;
             });
-            if (!matchingExport)
-              context.report(node, "Filename does not match any exports");
-          },
+            if (!matchingExport) context.report(node, 'Filename does not match any exports');
+          }
         };
-      },
-    },
-  },
+      }
+    }
+  }
 };
